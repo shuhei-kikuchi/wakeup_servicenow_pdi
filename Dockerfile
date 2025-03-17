618ccb2826732d4f5e135214e0567d522e9079b2
Shuhei <2855357+shuhei-kikuchi@users.noreply.github.com>
2025-03-17 16:28:57 +0900

    bot判定されないように諸々追加 & ID/パスワード等をconfigから取得するように変更

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM mcr.microsoft.com/playwright/python:v1.50.0-noble
-RUN pip install playwright
+RUN pip install playwright pyyaml
 RUN playwright install 
--- wakeup_snow_pdi.py
+++ wakeup_snow_pdi.py
@@ -1,34 +1,57 @@
 from playwright.sync_api import sync_playwright # type: ignore
+import time
+import yaml
 
 def run(playwright):
-    browser = playwright.chromium.launch(headless=True)
-    context = browser.new_context(ignore_https_errors=True)
+
+    with open('config.yaml', 'r') as yml:
+        conf = yaml.safe_load(yml)
+    # conf['servicenow']['email']
+    # conf['servicenow']['password']
+    # conf['servicenow']['instanceurl']
+
+
+    browser = playwright.chromium.launch(
+        headless=True,
+        args = [
+            "--disable-blink-features=AutomationControlled"
+            ]
+        )
+    
+    context = browser.new_context(
+        ignore_https_errors=True,
+        record_video_dir="logs/", #デバッグ用に動画を取得しているが、不要ならコメント
+        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"
+        )
 
     page = context.new_page()
-
     page.goto("https://developer.servicenow.com/dev.do")
+    time.sleep(3)
     with page.expect_navigation():
         page.click("button:has-text(\"Sign In\")")
-    page.goto("https://signon.service-now.com/x_snc_sso_auth.do?pageId=username")
+
+    time.sleep(3)
     page.locator("#email").click()
-    page.locator("#email").fill("XXXXXX")
+    time.sleep(2)
+    page.locator("#email").fill(conf['servicenow']['email'])
+    time.sleep(2)
     page.get_by_role("button", name="Next").click()
     page.locator("#password").click()
-    page.locator("#password").fill("XXXXX")
+    time.sleep(2)
+    page.locator("#password").fill(conf['servicenow']['password'])
+    time.sleep(2)
     with page.expect_navigation():
         page.get_by_role("button", name="Sign In").click()
-    
-    page.goto("https://developer.servicenow.com/dev.do")
 
-
-    with page.expect_navigation():
+    time.sleep(1)
+    with page.expect_popup() as popup_info:
         page.click("button:has-text(\"Start Building\")")
 
-    page.goto("https://devxxxxx.service-now.com")
-
-
-    page.screenshot(path="login.png")
-    page.close()
+    page1 = popup_info.value
+    page1.goto(conf['servicenow']['instanceurl'])
+    time.sleep(10) # インスタンス起動後の画面レイアウトを確認するために長めにsleep.動作確認用なので消してもOK
+    page1.screenshot(path="logs/login.png")  # ログインおよびインスタンス起動後の画面の確認
+    page1.close()
 
     page.close()
 
